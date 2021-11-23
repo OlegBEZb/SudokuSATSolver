9691e490df16e12529fdd49f433646c49c986568
Oleg Litvinov <olegolego@mail.ru>
2021-11-24 00:39:38 +0100

    added fullness heuristic and extracted utils

--- dpll.py
+++ dpll.py
@@ -3,18 +3,22 @@ from copy import deepcopy
 from itertools import chain
 from typing import List, Tuple
 from itertools import chain
-
+from utils import count_assignments, get_setup_fullness
 
 
 class DPLL:
     def __init__(self, clauses, variable_selection_method='random', verbose=0):
+
+        self.clauses = clauses
         # extracts unique variable from DIMACS format
-        self.variables_set = set(map(str, (map(abs, chain.from_iterable(clauses)))))
+        self.variables_set = set(map(str, (map(abs, chain.from_iterable(self.clauses)))))
         # converts into internal format where each literal looks like (var_name, True)
-        self.clauses = [[(str(abs(literal)), literal > 0) for literal in c] for c in clauses]
+        self.clauses = [[(str(abs(literal)), literal > 0) for literal in c] for c in self.clauses]
+
         self.variable_selection_method = variable_selection_method
-        self.verbose = verbose
         self.backtrack_counter = 0
+
+        self.verbose = verbose
 
     @staticmethod
     def neg(literal):
@@ -23,46 +27,47 @@ class DPLL:
     def select_random_variable(self, partial_assignment: List[Tuple]):
         if self.verbose:
             print('random variable selection')
+        if self.verbose > 4:
             print('inside variable selection partial_assignment', partial_assignment)
         already_split = set([literal[0] for literal in partial_assignment])
-        if self.verbose:
+        if self.verbose > 4:
             print('already_split', already_split)
         return random.choice(list(self.variables_set - already_split))
 
-    def select_MOM_variable(self, clauses: List[Tuple], partial_assignment):
-        k = 2
-        if self.verbose:
-            print('MOM variable selection')
-            print('inside MOM variable selection partial_assignment', partial_assignment)
-        already_split = set([literal[0] for literal in partial_assignment])
-        self.variables_set = self.variables_set - already_split
-        k = 2
-        clauses_length = []
-        for c in clauses:
-            clauses_length.append(len(c))
-        minimum_clauses = [j for j in clauses if len(j) == min(clauses_length)]
-        momlist = []
-        for i in self.variables_set:
-            occurences_positive = 0
-            occurences_negative = 0
-            for j in minimum_clauses:
-                # print((str(i), True))
-                # print(j)
-                true_literal = (str(i), True)
-                false_literal = (str(i), False)
-                if true_literal in j:
-                    occurences_positive += 1
-                if false_literal in j:
-                    occurences_negative += 1
-            # print(occurences_positive)
-            mom = (occurences_positive + occurences_negative) * math.pow(2,
-                                                                         k) + occurences_positive * occurences_negative
-            momlist.append(mom)
-        maxvalue = max(momlist)
-        maxmom = list(self.variables_set)[momlist.index(maxvalue)]
-        self.variables_set.remove(maxmom)
-
-        return maxmom
+    # def select_MOM_variable(self, clauses: List[Tuple], partial_assignment):
+    #     k = 2
+    #     if self.verbose:
+    #         print('MOM variable selection')
+    #         print('inside MOM variable selection partial_assignment', partial_assignment)
+    #     already_split = set([literal[0] for literal in partial_assignment])
+    #     self.variables_set = self.variables_set - already_split
+    #     k = 2
+    #     clauses_length = []
+    #     for c in clauses:
+    #         clauses_length.append(len(c))
+    #     minimum_clauses = [j for j in clauses if len(j) == min(clauses_length)]
+    #     momlist = []
+    #     for i in self.variables_set:
+    #         occurences_positive = 0
+    #         occurences_negative = 0
+    #         for j in minimum_clauses:
+    #             # print((str(i), True))
+    #             # print(j)
+    #             true_literal = (str(i), True)
+    #             false_literal = (str(i), False)
+    #             if true_literal in j:
+    #                 occurences_positive += 1
+    #             if false_literal in j:
+    #                 occurences_negative += 1
+    #         # print(occurences_positive)
+    #         mom = (occurences_positive + occurences_negative) * math.pow(2,
+    #                                                                      k) + occurences_positive * occurences_negative
+    #         momlist.append(mom)
+    #     maxvalue = max(momlist)
+    #     maxmom = list(self.variables_set)[momlist.index(maxvalue)]
+    #     self.variables_set.remove(maxmom)
+    #
+    #     return maxmom
 
     def get_literal_occurances(self, clauses: List[Tuple], partial_assignment: List[Tuple]):
         if self.verbose:
@@ -94,22 +99,54 @@ class DPLL:
         else:
             return (max_sum_variable, False)
 
-    def select_literal_MOM(self):
-        pass
+    def select_variable_fullness_based(self, clauses: List[Tuple], partial_assignment: List[Tuple]):
+        already_split = set([literal[0] for literal in partial_assignment])
+        if self.verbose > 4:
+            print('already_split', already_split)
+
+        available_variables = self.variables_set - already_split
+        row_assignments, col_assignments = count_assignments(partial_assignment)
+        if self.verbose:
+            print('in fullness based heuristic. row, col\n', row_assignments, col_assignments)
+
+        row_assignments = {k: v for k, v in row_assignments.items() if v != 9}
+        col_assignments = {k: v for k, v in col_assignments.items() if v != 9}
+
+        max_row_key = max(row_assignments, key=row_assignments.get)
+        max_col_key = max(col_assignments, key=col_assignments.get)
+
+        if row_assignments[max_row_key] > col_assignments[max_col_key]:
+            # find first digit == max_row_key in available_variables
+            target_index = 0
+            target_k = max_row_key
+        else:
+            # find second digit == max_col_key available_variables
+            target_index = 1
+            target_k = max_col_key
+
+        if self.verbose:
+            print('target_index', target_index, 'target_k', target_k)
+
+        # print('Available variables', sorted(available_variables))
+        for variable in available_variables:
+            if variable[target_index] == str(target_k):
+                print("Found variable based on fullness:", variable)
+                return variable
+        return self.select_random_variable(partial_assignment)
 
     def clause_simplication(self, clauses, literal: tuple):
         # copying for not to change them outside
         new_clauses = deepcopy(clauses)
 
-        if self.verbose:
+        if self.verbose > 2:
             print('before simplication', new_clauses)
         # delete clauses containing true literals
         new_clauses = [c for c in new_clauses if literal not in c]
-        if self.verbose:
+        if self.verbose > 2:
             print('simplified true literals', new_clauses)
         # shorten clauses containing false literals
         new_clauses = [[el for el in c if el != self.neg(literal)] for c in new_clauses]
-        if self.verbose:
+        if self.verbose > 2:
             print('shortened false literals', new_clauses)
 
         return new_clauses
@@ -142,7 +179,7 @@ class DPLL:
                 if len(clause) == 0:
                     return clauses, partial_assignment
                 elif len(clause) == 1:
-                    if self.verbose:
+                    if self.verbose > 1:
                         print("found unit clause", clause)
                     unit_literal = clause[0]
                     clauses = self.clause_simplication(clauses, unit_literal)
@@ -156,7 +193,7 @@ class DPLL:
         # copying the list of tuples for not to change them outside
         partial_assignment = deepcopy(partial_assignment)
 
-        if self.verbose:
+        if self.verbose > 2:
             print("\nBacktrack with partial_assignment", partial_assignment, 'and split_literal', split_literal)
         if split_literal != tuple():
             clauses = self.clause_simplication(clauses, split_literal)
@@ -181,14 +218,17 @@ class DPLL:
             if self.variable_selection_method == 'random':
                 split_variable = self.select_random_variable(partial_assignment)
                 split_literal = (split_variable, False)
+            elif self.variable_selection_method == 'fullness':
+                split_variable = self.select_variable_fullness_based(clauses, partial_assignment)
+                split_literal = (split_variable, True)
             elif self.variable_selection_method == 'JWOS':
                 split_literal = self.select_literal_JWOS(clauses, partial_assignment)
             elif self.variable_selection_method == 'JWTS':
                 split_literal = self.select_literal_JWTS(clauses, partial_assignment)
         except:
-            print('Can not split anymore')
-            return False, None
-
+            # print('Can not split anymore')
+            # return False, None
+            raise
 
         sat, assignments = self.backtrack(clauses, partial_assignment, split_literal)
         if not sat:
---
+++ utils.py
@@ -0,0 +1,74 @@
+from pysat.formula import CNF
+import math
+import numpy as np
+from collections import Counter
+
+
+def read_DIMACS(path):
+    return CNF(from_file=path).clauses
+
+
+def line2DIMACS(sudoku, inverse_input_mapping):
+    input_len = len(sudoku)
+    table_size = int(math.pow(input_len, 0.5))
+    clause_list = []
+    for row in range(table_size):
+        for column in range(table_size):
+            current_line_index = column + table_size * row
+            cell_value = sudoku[current_line_index]
+            if cell_value != '.':
+                clause_list.append(f"{inverse_input_mapping[row + 1]}{inverse_input_mapping[column + 1]}{cell_value} 0")
+    return clause_list, table_size
+
+
+def line2CNF(line, inverse_input_mapping):
+    cnf_list, table_size = line2DIMACS(line, inverse_input_mapping)
+    return CNF(from_string='\n'.join(cnf_list)).clauses, cnf_list, table_size
+
+
+def get_digit(number, n):
+    return int(number // 10 ** n % 10)
+
+
+def CNFsetup2matrix(sudoku_setup_CNF, sudoku_size):
+    matrix = np.zeros((sudoku_size, sudoku_size), dtype=int)
+    for clause in sudoku_setup_CNF:
+        for item in clause:
+            matrix[get_digit(item, 2) - 1, get_digit(item, 1) - 1] = get_digit(item, 0)
+    return matrix
+
+
+def get_setup_info(sudoku_str, sudoku_CNF, sudoku_size):
+    givens = Counter(sudoku_str.replace('\n', ''))
+    givens_num = sudoku_size ** 2 - givens['.']
+
+    col_fullness, row_fullness = get_setup_fullness(sudoku_CNF, sudoku_size)
+
+    return givens, givens_num, row_fullness, col_fullness
+
+
+def get_setup_fullness(sudoku_CNF, sudoku_size):
+    matrix = CNFsetup2matrix(sudoku_CNF, sudoku_size)
+    row_fullness = np.count_nonzero(matrix, axis=1)
+    row_fullness = {i: v for i, v in enumerate(row_fullness, 1)}
+    col_fullness = np.count_nonzero(matrix, axis=0)
+    col_fullness = {i: v for i, v in enumerate(col_fullness, 1)}
+    return col_fullness, row_fullness
+
+
+def count_assignments(partial_assignment):
+    row_assignments, col_assignments = dict((el, 0) for el in range(1, 10)), dict((el, 0) for el in range(1, 10))
+    for assignment in partial_assignment:
+        if assignment[1]:
+            item = assignment[0]
+            row_assignments[int(item[0])] += 1
+            col_assignments[int(item[1])] += 1
+    return row_assignments, col_assignments
+
+
+def matprint(mat, fmt="g"):
+    col_maxes = [max([len(("{:" + fmt + "}").format(x)) for x in col]) for col in mat.T]
+    for x in mat:
+        for i, y in enumerate(x):
+            print(("{:" + str(col_maxes[i]) + fmt + "}").format(y), end="  ")
+        print("")
